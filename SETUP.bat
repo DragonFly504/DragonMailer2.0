6a2f3383a48dc81b3577d9d1160196f079cb905f
Dragonfly504 <mky504@outlook.com>
2026-02-02 15:26:54 -0800

    Fix: Simpler desktop shortcut creation

--- SETUP.bat
+++ SETUP.bat
@@ -31,29 +31,21 @@ echo  [3/3] Creating Desktop shortcut...
 :: Get the desktop path
 set "DESKTOP=%USERPROFILE%\Desktop"
 
-:: Create a VBS script to make a proper shortcut
-echo Set oWS = WScript.CreateObject("WScript.Shell") > "%TEMP%\CreateShortcut.vbs"
-echo sLinkFile = "%DESKTOP%\Dragon Mailer.lnk" >> "%TEMP%\CreateShortcut.vbs"
-echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%TEMP%\CreateShortcut.vbs"
-echo oLink.TargetPath = "%~dp0Start_Dragon_Mailer.bat" >> "%TEMP%\CreateShortcut.vbs"
-echo oLink.WorkingDirectory = "%~dp0" >> "%TEMP%\CreateShortcut.vbs"
-echo oLink.Description = "Dragon Mailer - Bulk Email and SMS" >> "%TEMP%\CreateShortcut.vbs"
-echo oLink.IconLocation = "shell32.dll,14" >> "%TEMP%\CreateShortcut.vbs"
-echo oLink.Save >> "%TEMP%\CreateShortcut.vbs"
-
-cscript //nologo "%TEMP%\CreateShortcut.vbs"
-del "%TEMP%\CreateShortcut.vbs"
+:: Create a simple batch file shortcut on desktop instead
+echo @echo off > "%DESKTOP%\Dragon Mailer.bat"
+echo cd /d "%~dp0" >> "%DESKTOP%\Dragon Mailer.bat"
+echo start "" "%~dp0Start_Dragon_Mailer.bat" >> "%DESKTOP%\Dragon Mailer.bat"
 
 echo.
 echo  ========================================
 echo     [OK] Setup Complete!
 echo  ========================================
 echo.
-echo  A shortcut "Dragon Mailer" has been
+echo  A shortcut "Dragon Mailer.bat" has been
 echo  created on your Desktop.
 echo.
 echo  Double-click it to start the app!
 echo.
-echo  Go to Settings to enable login protection.
+echo  Login: admin
 echo.
 pause
